c8a2d04e45ff6e1eb4b7cf027c77b372c1809581
Julian <julian.limburg@gmx.de>
2022-06-12 21:14:32 +0200

    Added Tester

---
+++ src/tester.py
@@ -0,0 +1,17 @@
+import os
+import PAss
+
+# Get Test source files
+path = os.path.join(os.path.dirname(__file__), "../tests/in/")
+test_files = os.listdir(path)
+
+# Run Tests
+for test_file in test_files:
+    print(f"Running Test: {test_file}")
+    path_in = os.path.join(os.path.dirname(__file__),
+                           "../tests/in/") + test_file
+    test = PAss.read_input_file(path_in)
+
+    path_out = os.path.join(os.path.dirname(__file__),
+                            "../tests/out/") + test_file.replace('.pass', '.ass')
+    PAss.compile(test, test_file.replace(".pass", ""), path_out)
